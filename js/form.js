5abaf074dfb0c0a0cfcce3d6b45f0a20cc45b680
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2022-08-12 10:34:51 +0100

    form - some more changes
    I removed one of form's field and replaced the other one with "full name". I did the same in my JavaScript. I removed one of variables and one function. I changed the other function, added a regex for full mane, modified some manes and texts inside that function and in variable's name. I resolved the issue with the submit button that was refreshing the page and prevented the last function from running.

--- js/form.js
+++ js/form.js
@@ -1,37 +1,23 @@
 // form validation script for the form in the contact page (why it doesn't work?)
-var firstNameError = document.getElementById("first-name-error");
-var lastNameError = document.getElementById("last-name-error");
+var nameError = document.getElementById("name-error");
 var emailError = document.getElementById("email-error");
 var subjectError = document.getElementById("subject-error");
 var messageError = document.getElementById("message-error");
+var submitError = document.getElementById("submit-error");
 var form = document.getElementById("contact-form");
 
-function validateFirstName() {
-    var firstName = document.getElementById("contact-first-name").value;
+function validateName() {
+    var name = document.getElementById("contact-name").value;
 
-    if (firstName.length == 0) {
-        firstNameError.innerHTML = "*First name is required";
+    if (name.length == 0) {
+        nameError.innerHTML = "*Full name is required";
         return false;
     }
-    if (!firstName.match(/^[a-zA-Z]+$/)) {
-        firstNameError.innerHTML = "*First name must contain only letters";
+    if (!name.match(/^[a-zA-Z]+ [a-zA-Z]+$/)) {
+        nameError.innerHTML = "*Name must contain only letters";
         return false;
     }
-    lastNameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-    return true;
-}
-function validateLastName() {
-    var lastName = document.getElementById("contact-last-name").value;
-
-    if (lastName.length == 0) {
-        lastNameError.innerHTML = "*Last name is required";
-        return false;
-    }
-    if (!lastName.match(/^[a-zA-Z]+$/)) {
-        lastNameError.innerHTML = "*Last name must contain only letters";
-        return false;
-    }
-    lastNameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
     return true;
 }
 function validateEmail() {
@@ -69,7 +55,7 @@ function validateMessage() {
     return true;
 }
 function validateSubmit() {
-    if (validateFirstName() && validateLastName() && validateEmail() && validateSubject() && validateMessage()) {
+    if (validateName() && validateEmail() && validateSubject() && validateMessage()) {
         submitError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
         return true;
     }
