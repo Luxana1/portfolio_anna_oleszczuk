5f0444462d94dcd5ede27f9be1341a8e51e7af85
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2022-08-12 08:58:41 +0100

    changes in form
    I made a bug changes in my form again. in my html, I added  "onkeyup" to my inputs, I replaced divs error messages with spans error messages. I also added success icons to my js function that I wrote again and now everything work as expected. The only problem is that the button automatically refreshes the page but it's not the issue if JavaScript, the issue is somewhere else. Need resolving. Also styling for error messages and success icons need to be added.

--- js/form.js
+++ js/form.js
@@ -1,51 +1,81 @@
 // form validation script for the form in the contact page (why it doesn't work?)
-const form = document.querySelector("form[name='myForm']");
-const firstNameInput = document.querySelector("input[name='first_name']");
-const lastNameInput = document.querySelector("input[name='last_name']");
-const emailInput = document.querySelector("input[name='email']");
-const subjectInput = document.querySelector("input[name='subject']");
-const messageInput = document.querySelector("textarea[name='message']");
+var firstNameError = document.getElementById("first-name-error");
+var lastNameError = document.getElementById("last-name-error");
+var emailError = document.getElementById("email-error");
+var subjectError = document.getElementById("subject-error");
+var messageError = document.getElementById("message-error");
+var form = document.getElementById("contact-form");
 
-firstNameInput.isValid = () => !!firstNameInput.ariaValueMax;
-lastNameInput.isValid = () => !!lastNameInput.ariaValueMax;
-emailInput.isValid = () => isValidEmail(emailInput.value);
-subjectInput.isValid = () => !!subjectInput.ariaValueMax;
-messageInput.isValid = () => !!messageInput.ariaValueMax;
+function validateFirstName() {
+    var firstName = document.getElementById("contact-first-name").value;
 
+    if (firstName.length == 0) {
+        firstNameError.innerHTML = "*First name is required";
+        return false;
+    }
+    if (!firstName.match(/^[a-zA-Z]+$/)) {
+        firstNameError.innerHTML = "*First name must contain only letters";
+        return false;
+    }
+    lastNameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+function validateLastName() {
+    var lastName = document.getElementById("contact-last-name").value;
 
-const isValidEmail = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-};
+    if (lastName.length == 0) {
+        lastNameError.innerHTML = "*Last name is required";
+        return false;
+    }
+    if (!lastName.match(/^[a-zA-Z]+$/)) {
+        lastNameError.innerHTML = "*Last name must contain only letters";
+        return false;
+    }
+    lastNameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+function validateEmail() {
+    var email = document.getElementById("contact-email").value;
 
+    if (email.length == 0) {
+        emailError.innerHTML = "*Email is required";
+        return false;
+    }
+    if (!email.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+        emailError.innerHTML = "*Email is not valid";
+        return false;
+    }
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+function validateSubject() {
+    var subject = document.getElementById("contact-subject").value;
 
-// let showValidate = false;
-// let isFormValid = false;
+    if (subject.length == 0) {
+        subjectError.innerHTML = "*Subject is required";
+        return false;
+    }
+    subjectError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+function validateMessage() {
+    var message = document.getElementById("contact-message").value;
 
-// const validateInputs = () => {
-//     console.log("bla bla it works");
-//     if (!shouldValidate) return;
-
-//     isFormValid = true;
-//     inputFields.forEach(input => {
-//         input.classList.remove("invalid");
-//         input.nextElementSibling.classList.remove("hide");
-
-//         if (!input.isValid()) {
-//             input.classList.add("invalid");
-//             isFormValid = false;
-//             input.nextElementSibling.classList.add("hide");
-//         }
-//     });
-// };
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    shouldValidate = true;
-    validateInputs();
-    if (isFormValid) {
-        form.submit();
-    }   
-});
-console.log("Hope, this one also works");
-inputFields.forEach((input) => input.addEventListener("input", validateInputs));
+    if (message.length == 0) {
+        messageError.innerHTML = "*Message is required";
+        return false;
+    }
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+function validateSubmit() {
+    if (validateFirstName() && validateLastName() && validateEmail() && validateSubject() && validateMessage()) {
+        submitError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+        return true;
+    }
+    submitError.innerHTML = "*Please fill out all fields";
+    submitError.style.display = "block";
+    setTimeout(function(){submitError.style.display = "none";}, 3000);
+    return false;
+}
+console.log("script.js loaded");
--- js/menu.js
+++ js/menu.js
@@ -9,25 +9,25 @@ function myMenu() {
 }
 
 // function that disable myMenu on the screen greater than 768px
-function myMenu() { 
-    if (window.innerWidth > 768) {
-        document.getElementById("myLinks").style.display = "none";
-    } else {
-        document.getElementById("myLinks").style.display = "block";
-    }
-}
+// function myMenu() { 
+//     if (window.innerWidth > 768) {
+//         document.getElementById("myLinks").style.display = "none";
+//     } else {
+//         document.getElementById("myLinks").style.display = "block";
+//     }
+// }
 
 //disable the onclick event on the <a> element with the id="myLinks" on the screen greater than 768px
-function myMenu() {
-    if (window.innerWidth > 768) {
-        document.getElementById("myLinks").onclick = function() {
-            return false;
-        }
-    } else {
-        document.getElementById("myLinks").onclick = function() {
-            return true;
-        }
-    }
-}
+// function myMenu() {
+//     if (window.innerWidth > 768) {
+//         document.getElementById("myLinks").onclick = function() {
+//             return false;
+//         }
+//     } else {
+//         document.getElementById("myLinks").onclick = function() {
+//             return true;
+//         }
+//     }
+// }
 
 // not sure how to make only the div to be displayed on the screen greater than 768px
