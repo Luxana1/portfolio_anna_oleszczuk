bf8830c6717d0fb732f1ec6609581f776b83359c
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2022-08-17 10:08:11 +0100

    myMenu() function
    I modified the function by adding the specific screen size the function has to be enabled on. I also removed the other function that supposed to disable the previous one when the screen size reaches the width of 768 and replaced it with a working one.

--- js/menu.js
+++ js/menu.js
@@ -1,33 +1,22 @@
 // menu navigation
 function myMenu() {
     var x = document.getElementById("myLinks")
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
+    if (window.innerWidth < 768) {
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
     }
 }
-
-// function that disable myMenu on the screen greater than 768px
-// function myMenu() { 
-//     if (window.innerWidth > 768) {
-//         document.getElementById("myLinks").style.display = "none";
-//     } else {
-//         document.getElementById("myLinks").style.display = "block";
-//     }
-// }
-
-//disable the onclick event on the <a> element with the id="myLinks" on the screen greater than 768px
-// function myMenu() {
-//     if (window.innerWidth > 768) {
-//         document.getElementById("myLinks").onclick = function() {
-//             return false;
-//         }
-//     } else {
-//         document.getElementById("myLinks").onclick = function() {
-//             return true;
-//         }
-//     }
-// }
+// when the screen is greater than 768px, remove display:mone from the menu
+window.addEventListener("resize", myMenu);
+    var x = document.getElementById("myLinks")
+    if (window.innerWidth >= 768) {
+        x.style.display = "block";
+    } else {
+        (window.innerWidth < 768)
+        x.style.display = "none";
+    }
 
 // not sure how to make only the div to be displayed on the screen greater than 768px
