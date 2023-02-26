7eeaf683ca025a32b0dbd51457ae0bd13bb2fa27
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2023-02-26 13:44:47 +0000

    Typing effect js update
    I updated the typing effect JavaScript. I made changes that stop the colorChange function when the message is finished. When colorChange stops, the message has a random color that is not changing untill the page is reloaded.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
 // typing effect for the hero text
-var messageArray = ["My Name is Anna Oleszczuk. " + "<br>" + "I am a Web Developer."];
-var textPosition = 0;
-var speed = 150;
-var randomColor = Math.floor(Math.random()*16777215).toString(16);
+const messageArray = ["My Name is Anna Oleszczuk. <br> I am a Web Developer."];
+let textPosition = 0;
+const speed = 150;
 
-// typing effect for the hero text 
+const message = () => {
+  document.querySelector("#text").innerHTML = messageArray[0].substring(0, textPosition);
 
-message = () => {
-    document.querySelector("#text").innerHTML = messageArray[0].substring(0, textPosition);
-
-    if(textPosition++ != messageArray[0].length)
-        setTimeout(message, speed); 
+  if (textPosition++ != messageArray[0].length) {
+    setTimeout(message, speed); 
+  } else {
+    // stop color change when typing effect is finished
+    clearInterval(colorInterval);
+  }
 }
+
 window.addEventListener("load", message);
 
-// color change for the each character of hero text while the message is typing
-
-var colorChange = () => {   
-    var text = document.querySelector("#text");
-    text.style.color = "#" + randomColor;
-    randomColor = Math.floor(Math.random()*16777215).toString(16);
+const colorChange = () => {   
+  const text = document.querySelector("#text");
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  text.style.color = "#" + randomColor;
 }
-setInterval(colorChange, speed);
+
+// call colorChange every 200ms
+const colorInterval = setInterval(colorChange, 200);
 
 
 
