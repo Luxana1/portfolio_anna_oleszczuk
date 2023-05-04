521592ac5e326e819edbbd1fdf313e32d8b458a4
Luxana1 <96484101+Luxana1@users.noreply.github.com>
2023-05-04 10:53:55 +0100

    JavaScript and header SCSS
    I updated the JavaScript function for the dropdown menu. I got rid of the unwanted code from Header SCSS.

--- js/menu.js
+++ js/menu.js
@@ -1,36 +1,29 @@
-// // menu navigation
-// function myMenu() {
-//     var x = document.getElementById("myLinks")
-//     if (window.innerWidth < 768) {
-//         if (x.style.display === "block") {
-//             x.style.display = "none";
-//         } else {
-//             x.style.display = "block";
-//         }
-//     }
-// }
 // menu navigation
 function myMenu() {
     const x = document.getElementById("myLinks");
     const icon = document.querySelector(".icon i");
     
-    if (window.innerWidth < 768) {
-      if (x.style.display === "block") {
-        // close the menu with a fade out animation
-        x.style.animation = "fadeOut 0.5s";
-        setTimeout(() => x.style.display = "none", 500);
-        icon.classList.remove("fa-times");
-        icon.classList.add("fa-bars");
-      } else {
-        // open the menu with a fade in animation
-        x.style.animation = "fadeIn 0.5s";
-        x.style.display = "block";
-        icon.classList.remove("fa-bars");
-        icon.classList.add("fa-times");
-      }
-    } else {
-      x.style.display = "block";
-    }
+    x.classList.toggle("show-menu");
+    icon.classList.toggle("fa-times");
+    icon.classList.toggle("fa-bars");
+
+    // if (window.innerWidth < 768) {
+    //   if (x.style.display === "block") {
+    //     // close the menu with a fade out animation
+    //     x.style.animation = "fadeOut 0.5s";
+    //     setTimeout(() => x.style.display = "none", 500);
+    //     icon.classList.remove("fa-times");
+    //     icon.classList.add("fa-bars");
+    //   } else {
+    //     // open the menu with a fade in animation
+    //     x.style.animation = "fadeIn 0.5s";
+    //     x.style.display = "block";
+    //     icon.classList.remove("fa-bars");
+    //     icon.classList.add("fa-times");
+    //   }
+    // } else {
+    //   x.style.display = "block";
+    // }
   }
   
 
